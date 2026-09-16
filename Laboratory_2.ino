629793b50781c3db747bea9596079b4f3f10e2b5
Shyane Novelle Canayan <shyanenovelle.canayan@g.msuiit.edu.ph>
2026-09-16 23:46:30 +0800

    Add files via upload

---
+++ Laboratory_2.ino
@@ -0,0 +1,26 @@
+int getLampBrightness(bool switchOn, int brightnessSetting) {
+  if (switchOn) {
+    return brightnessSetting;
+  } else {
+    return 0;
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+
+  int brightnessSetting = 0;
+  bool switchOn = true; 
+
+  int lampBrightness = getLampBrightness(switchOn, brightnessSetting);
+
+  Serial.print("Brightness setting: ");
+  Serial.println(brightnessSetting);
+
+  Serial.print("Lamp brightness: ");
+  Serial.println(lampBrightness);
+}
+
+void loop() {
+}
